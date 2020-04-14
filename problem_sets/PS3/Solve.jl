c29eef3057de4c02262ba26ea9ead9eb508cadd4
Brian Davey <bd395@cornell.edu>
2020-04-14 10:45:33 -0400

    Update Solve.jl

--- problem_sets/PS3/Solve.jl
+++ problem_sets/PS3/Solve.jl
@@ -6,5 +6,6 @@ data_dictionary = maximize_urea_production_open(0,0,0);
 
 # solve the lp problem -
 (objective_value, flux_array, dual_array, uptake_array, exit_flag) = calculate_optimal_flux_distribution(data_dictionary);
+objective_value = -objective_value
 
-println("Max urea production: ", -objective_value, "mmol/gdw*hr")
+println("Max urea production: ", objective_value, "mmol/gdw*hr")

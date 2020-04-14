fd9132d4ce63561d25048200b33449f618e23a09
Brian Davey <bd395@cornell.edu>
2020-04-14 10:18:34 -0400

    Update Solve.jl

--- problem_sets/PS3/Solve.jl
+++ problem_sets/PS3/Solve.jl
@@ -2,7 +2,9 @@ include("Include.jl")
 
 # load the data dictionary -
 # data_dictionary = maximize_urea_production(0,0,0)
-data_dictionary = maximize_urea_production_open(0,0,0)
+data_dictionary = maximize_urea_production_open(0,0,0);
 
 # solve the lp problem -
-(objective_value, flux_array, dual_array, uptake_array, exit_flag) = calculate_optimal_flux_distribution(data_dictionary)
+(objective_value, flux_array, dual_array, uptake_array, exit_flag) = calculate_optimal_flux_distribution(data_dictionary);
+
+println("Max urea production: ", -objective_value, "mmol/gdw*hr")

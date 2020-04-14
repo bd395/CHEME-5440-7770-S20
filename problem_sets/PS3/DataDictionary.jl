631236d7877032b9e7ce36121658570d9f5dc80b
Brian Davey <bd395@cornell.edu>
2020-04-13 22:14:39 -0400

    Update DataDictionary.jl

--- problem_sets/PS3/DataDictionary.jl
+++ problem_sets/PS3/DataDictionary.jl
@@ -206,12 +206,12 @@ end
 function DataDictionary(time_start,time_stop,time_step)
 
 	# Load the stoichiometric network from disk -
-	stoichiometric_matrix = readdlm("Stoich_Matrix.dat")#("Network.dat");
+	stoichiometric_matrix = readdlm("Stoich_Matrix.dat");
 
 	# What is the system dimension? -
 	(number_of_species,number_of_reactions) = size(stoichiometric_matrix)
 
-	E = 0.01E-5
+	E = 1E-5
 
 	kcat_v1 = 203*3600
 	kcat_v2 = 34.5*3600
@@ -230,41 +230,39 @@ function DataDictionary(time_start,time_stop,time_step)
 
 	E = (0.01)*(1/1000)	# mmol/gDW
 	metabolic_vmax_array = [
-		203*(3600)*E	;	# v1 ec:6.3.4.5 mmol/gDW-hr
-		34.5*(3600)*E	;	# v2 ec:4.3.2.1 mmol/gDW-hr
-		249*(3600)*E	;	# v3 ec:3.5.3.1 mmol/gDW-hr
-		88.1*(3600)*E	;	# v4 ec:2.1.3.3 mmol/gDW-hr
-		13.7*(3600)*E	;	# v5 ec:1.14.13.39 mmol/gDW-hr
-		13.7*(3600)*E	;	# v6 ec:1.14.13.39 mmol/gDW-hr
-
+		v1_max*(4.67E-3/(4.67E-3 + 3.92E-4))*(1.49E-2/(1.49E-2 + 1.54E-4))	;	# v1 ec:6.3.4.5 mmol/gDW-hr
+		v2_max*1	;	# v2 ec:4.3.2.1 mmol/gDW-hr
+		v3_max*2.55E-4/(1.55E-3 + 2.55E-4)	;	# v3 ec:3.5.3.1 mmol/gDW-hr
+		v4_max*1.01E-5/(1.01E-5 + 8.50E-4)	;	# v4 ec:2.1.3.3 mmol/gDW-hr
+		v5f_max*2.55E-4/(3.50E-06 + 2.55E-4)	;	# v5 ec:1.14.13.39 mmol/gDW-hr
+		v5r_max* 1;	# v6 ec:1.14.13.39 mmol/gDW-hr
+		];
 
 
 	# Setup default flux bounds array -
 	default_bounds_array = [
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 1	M_ATP_c+M_L-Citrulline_c+M_L-Aspartate_c --> M_AMP_c+M_Diphosphate_c+M_N-(L-Arginino)succinate_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 2	M_N-(L-Arginino)succinate_c --> M_Fumarate_c+M_L-Arginine_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 3	M_L-Arginine_c+M_H2O_c --> M_L-Ornithine_c+M_Urea_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 4	M_Carbamoyl_phosphate_c+M_L-Ornithine_c --> M_Orthophosphate_c+M_L-Citrulline_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 5	2.0*M_L-Arginine_c+4.0*M_Oxygen_c+3.0*M_NADPH_c+3.0*M_H_c --> 2.0*M_Nitric_oxide_c+2.0*M_L-Citrulline_c+3.0*M_NADP_c+4.0*M_H2O_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 6	2.0*M_Nitric_oxide_c+2.0*M_L-Citrulline_c+3.0*M_NADP_c+4.0*M_H2O_c --> 2.0*M_L-Arginine_c+4.0*M_Oxygen_c+3.0*M_NADPH_c+3.0*M_H_c
-
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 7	[] --> M_Carbamoyl_phosphate_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 8	[] --> M_L-Aspartate_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 9	M_Fumarate_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 10	M_Urea_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 11	[] --> M_ATP_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 12	M_AMP_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 13	M_Diphosphate_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 14	M_Orthophosphate_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 15	[] --> M_Oxygen_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 16	[] --> M_NADPH_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 17	[] --> M_H_c
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 18	M_Nitric_oxide_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 19	M_NADP_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 20	M_H2O_c --> []
-		0	2.2148976	;	# Vmax [mmol/gdw-hr] 21	[] --> M_H2O_c
-	];
-
+		0	metabolic_vmax_array[1]	;	# Vmax [mmol/gdw-hr] 1	M_ATP_c+M_L-Citrulline_c+M_L-Aspartate_c --> M_AMP_c+M_Diphosphate_c+M_N-(L-Arginino)succinate_c
+		0	metabolic_vmax_array[2]	;	# Vmax [mmol/gdw-hr] 2	M_N-(L-Arginino)succinate_c --> M_Fumarate_c+M_L-Arginine_c
+		0	metabolic_vmax_array[3]	;	# Vmax [mmol/gdw-hr] 3	M_L-Arginine_c+M_H2O_c --> M_L-Ornithine_c+M_Urea_c
+		0	metabolic_vmax_array[4]	;	# Vmax [mmol/gdw-hr] 4	M_Carbamoyl_phosphate_c+M_L-Ornithine_c --> M_Orthophosphate_c+M_L-Citrulline_c
+		0	metabolic_vmax_array[5]	;	# Vmax [mmol/gdw-hr] 5	2.0*M_L-Arginine_c+4.0*M_Oxygen_c+3.0*M_NADPH_c+3.0*M_H_c --> 2.0*M_Nitric_oxide_c+2.0*M_L-Citrulline_c+3.0*M_NADP_c+4.0*M_H2O_c
+		0	metabolic_vmax_array[6]	;	# Vmax [mmol/gdw-hr] 6	2.0*M_Nitric_oxide_c+2.0*M_L-Citrulline_c+3.0*M_NADP_c+4.0*M_H2O_c --> 2.0*M_L-Arginine_c+4.0*M_Oxygen_c+3.0*M_NADPH_c+3.0*M_H_c
+		0	10	;	# Vmax [mmol/gdw-hr] 7	[] --> M_Carbamoyl_phosphate_c
+		0	10	;	# Vmax [mmol/gdw-hr] 8	[] --> M_L-Aspartate_c
+		0	10	;	# Vmax [mmol/gdw-hr] 9	M_Fumarate_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 10	M_Urea_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 11	[] --> M_ATP_c
+		0	10	;	# Vmax [mmol/gdw-hr] 12	M_AMP_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 13	M_Diphosphate_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 14	M_Orthophosphate_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 15	[] --> M_Oxygen_c
+		0	10	;	# Vmax [mmol/gdw-hr] 16	[] --> M_NADPH_c
+		0	10	;	# Vmax [mmol/gdw-hr] 17	[] --> M_H_c
+		0	10	;	# Vmax [mmol/gdw-hr] 18	M_Nitric_oxide_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 19	M_NADP_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 20	M_H2O_c --> []
+		0	10	;	# Vmax [mmol/gdw-hr] 21	[] --> M_H2O_c
+	 ];
 
 	# Setup default species bounds array -
 	species_bounds_array = [
@@ -286,7 +284,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		0.0	0.0	;	# 16 H
 		0.0	0.0	;	# 17 Nitric Oxide
 		0.0	0.0	;	# 18 NADP
-	];
+	 ];
 
 	# Min/Max flag - default is minimum -
 	is_minimum_flag = true
@@ -303,7 +301,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		0.0	;	# b1
 		0.0	;	# b2
 		0.0	;	# b3
-		1.0	;	# b4 - Urea
+		-1.0	;	# b4 - Urea
 		0.0	;	# b5
 		0.0	;	# b6
 		0.0	;	# b7
@@ -316,7 +314,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		0.0	;	# b14
 		0.0	;	# b15
 
-	];
+	 ];
 
 	# List of reation strings - used to write flux report
 	list_of_reaction_strings = [
@@ -341,7 +339,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		"b13::M_NADP_c --> []"	;	# 19
 		"b14::M_H2O_c --> []"	;	# 20
 		"b14_reverse::[] --> M_H2O_c"	;	# 21
-	];
+	 ];
 
 	# List of metabolite strings - used to write flux report
 	list_of_metabolite_symbols = [
@@ -363,7 +361,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		"M_Orthophosphate_c"	;	# 16
 		"M_Oxygen_c"	;	# 17
 		"M_Urea_c"	;	# 18
-	];
+	 ];
 
 
 	# Metabolic Vmax array (units: mmol/B-hr) -
@@ -389,7 +387,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		2.2148976	;	# Vmax [mmol/gdw-hr] 19	M_NADP_c --> []
 		2.2148976	;	# Vmax [mmol/gdw-hr] 20	M_H2O_c --> []
 		2.2148976	;	# Vmax [mmol/gdw-hr] 21	[] --> M_H2O_c
-	];
+	 ];
 
 	# Metabolic saturation constant array (units mM) -
 	number_of_metabolic_rates = length(metabolic_vmax_array)
@@ -494,7 +492,7 @@ function DataDictionary(time_start,time_stop,time_step)
 		0.0	;	# 16 M_Orthophosphate_c
 		0.0	;	# 17 M_Oxygen_c
 		0.0	;	# 18 M_Urea_c
-	];
+	 ];
 
 	# =============================== DO NOT EDIT BELOW THIS LINE ============================== #
 	data_dictionary = Dict{AbstractString,Any}()

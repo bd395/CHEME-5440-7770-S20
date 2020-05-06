29ae7cbad5672587dbb0899de0aaf1f3e27eab69
Brian Davey <bd395@cornell.edu>
2020-05-06 12:56:30 -0400

    Create PS4_3.jl

---
+++ problem_sets/PS4/PS4_3.jl
@@ -0,0 +1,28 @@
+include("PhasePortraitV2_3.jl")
+
+# Function for a dual repression system without cooperativity
+# x1: range of x1 values (i.e. A values)
+# x2: range of x2 values (i.e. R values)
+# We use `@.` to apply the calculations across all rows.
+# Note that model parameters are specified within the function
+# Returns computed (dx1/dt, dx2/dt) over the range of (x1, x2)
+function toggleMono(u, v)
+    alpha = 10.0               #degradation rate const. for repressor 1
+    n = 2.0               #degradation rate const. for repressor 2
+
+    udot = @. -u + alpha/(1+v^n)
+    vdot = @. -v + alpha/(1+u^n)   #eqn 12
+
+    return (udot,vdot)
+end
+
+
+#Range of x1, x2 values
+x1range = (0,15,15)          #Has the form (min, max, num points)
+x2range = (0,15,15)          #Has the form (min, max, num points)
+x₀ = ([1.0, 10.0],)  #initial state vectors; a common must be included after the first array
+tspan=(0.0,30.0)             #time span
+
+#Call the phaseplot functon to construct the phase portrait
+phaseplot(toggleMono, x1range, x2range, xinit=(), t=tspan, clines=true,
+        norm=true, scale=0.5)

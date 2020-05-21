9cc264d6cc9342e22c7b22002d4a346c1bef7c03
Brian Davey <bd395@cornell.edu>
2020-05-21 17:00:27 -0400

    Update prob1B.jl

--- Final/prob1B.jl
+++ Final/prob1B.jl
@@ -6,28 +6,28 @@ include("phasePortrait.jl")
 # We use `@.` to apply the calculations across all rows.
 # Note that model parameters are specified within the function
 # Returns computed (dx1/dt, dx2/dt) over the range of (x1, x2)
-function notchdelta(dNdt, N1, N2)
+function notchdelta(D1, D2)
     # D1 = 1
     # D2 = 1
 
-    dNdt[1] = (1/(1+10*N2^2))/(0.1 + (1/(1+10*N2^2)))
-    dNdt[2] = (1/(1+10*N1^2))/(0.1 + (1/(1+10*N1^2)))
+    dDdt1 = @. 1/(0.1 + 10*(D2^2/(0.1 + D2^2))) - D1
+    dDdt2 = @. 1/(0.1 + 10*(D1^2/(0.1 + D1^2))) - D2
 
     # N1dot = @. - N1
     # N2dot = @. - N2
 
-    return dNdt #(N1dot, N2dot)
+    return (dDdt1, dDdt2)
 end
 
 #Range of x1, x2 values
-N1_range = (-20,20,11)          #Has the form (min, max, num points)
-N2_range = (-20,20,11)          #Has the form (min, max, num points)
-n0 = ([1.0,4.0],)
+D1_range = (0,1.2,10)          #Has the form (min, max, num points)
+D2_range = (0,1.2,10)          #Has the form (min, max, num points)
+d0 = ([0.75,0.50],)
 #x₀ = ([1.0, 10.0],)  #initial state vectors; a common must be included after the first array
 tspan=(0.0,30.0)             #time span
 #print(length(tspan))
 #Call the phaseplot functon to construct the phase portrait
-phaseplot(notchdelta, N1_range, N2_range, xinit=n0 , t=tspan, clines=true,
+phaseplot(notchdelta, D1_range, D2_range, xinit=d0 , t=tspan, clines=true,
         norm=true, scale=0.5)
 
 #plot(t, sol, vars=(1))
